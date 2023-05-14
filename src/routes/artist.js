07c91b3b064e91eb20bd90a5a238c5c0b8fdfd05
David Ding <dingtechxing16@gmail.com>
2023-05-14 21:54:59 +0100

    Update and delete artists

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -5,4 +5,7 @@ const artistRouter = express.Router();
 artistRouter.post("/", artistController.createArtist);
 artistRouter.get("/", artistController.readArtist);
 artistRouter.get("/:id", artistController.getArtistById);
+artistRouter.put("/:id", artistController.putArtist);
+artistRouter.patch("/:id", artistController.patchArtist);
+artistRouter.delete("/:id", artistController.deleteArtist);
 module.exports = artistRouter;
